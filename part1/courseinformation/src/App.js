9c9f9daf5ebb166811ebff98e40837c8d42a6ad9
Thant Ko Zaw <thantmkou10@gmail.com>
2022-07-21 17:35:15 +0630

    Add courseinformation

---
+++ part1/courseinformation/src/App.js
@@ -0,0 +1,61 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const numberOfExercises = parts.reduce(
+    (prev, curr) => prev + curr.exercises,
+    0,
+  );
+
+  return <p>Number of exercises {numberOfExercises}</p>;
+};
+
+export default App;
